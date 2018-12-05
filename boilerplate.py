5f9c5a0d0fc770b067c6ac1ee18d032ee48282cc
Oliver Sherouse <oliver@oliversherouse.com>
2018-12-05 11:09:45 -0500

    final changes for v2

--- boilerplate.py
+++ boilerplate.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python
-"""A boilerplate script to be customized for data projects.
+#!/usr/bin/env python3
+"""
+A boilerplate script to be customized for data projects.
 
 This script-level docstring will double as the description when the script is
 called with the --help or -h option.
-
 """
 
 # Standard Library imports
 import argparse
+# import collections
+# import csv
+# import itertools
 import logging
 
 # External library imports
 # import pandas as pd
 # import numpy as np
-
 
 # Standard Library from-style imports go here
 from pathlib import Path
@@ -21,14 +23,10 @@ from pathlib import Path
 # External library from-style imports go here
 # from matplotlib import pyplot as plt
 
-# Ideally we all live in a unicode world, but if you have to use something
-# else, you can set it here
-ENCODE_IN = 'utf-8'
-ENCODE_OUT = 'utf-8'
+__version__ = '0.1'
 
-log = logging.getLogger(
-    __name__ if __name__ != '__main__ ' else Path(__file__).stem
-)
+log = logging.getLogger(__name__ if __name__ != '__main__ '
+                        else Path(__file__).stem)
 
 
 def manipulate_data(data):
@@ -47,22 +45,33 @@ def manipulate_data(data):
 
 def parse_args():
     """Parse command line arguments."""
-    inft = argparse.FileType(encoding=ENCODE_IN)
-    outft = argparse.filetype('w', encoding=ENCODE_OUT)  # , newline='')
-
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('infile', nargs='?', type=inft, default=inft('-'))
-    parser.add_argument('-o', '--outfile', type=outft, default=outft('-'))
+    parser.add_argument('infile', nargs='?', default='-')
+    parser.add_argument('-ei', '--infile_encoding', default='utf-8')
+    parser.add_argument('-o', '--outfile', default='-')
+    parser.add_argument('-eo', '--outfile_encoding', default='utf-8')
+
     verbosity = parser.add_mutually_exclusive_group()
     verbosity.add_argument('-v', '--verbose', action='store_const',
                            const=logging.DEBUG, default=logging.INFO)
     verbosity.add_argument('-q', '--quiet', dest='verbose',
                            action='store_const', const=logging.WARNING)
-    return parser.parse_args()
+
+    parser.add_argument('--version', action='version',
+                        version=f'%(prog)s v{__version__}')
+
+    args = parser.parse_args()
+    args.infile = argparse.FileType(encoding=args.infile_encoding)(args.infile)
+    args.outfile = argparse.FileType(
+        mode='w',
+        encoding=args.outfile_encoding,
+        # newline='', # for csvs
+    )(args.outfile)
+    return args
 
 
 def read_instream(instream):
-    """Convert raw input for to a manipulatable format.
+    """Convert raw input for to a manipulable format.
 
     Arguments:
 
@@ -71,6 +80,7 @@ def read_instream(instream):
     Returns: probably a DataFrame
 
     """
+    log.info('Reading Input')
     return instream.read()
 
 
